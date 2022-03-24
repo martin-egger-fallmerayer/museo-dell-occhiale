9c711a2f5675c40342592332cf20a33fb71bda97
dv <none>
2022-03-24 16:39:38 +0100

    added bootstrap to client

--- dvdb-client/src/App.tsx
+++ dvdb-client/src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from "react-bootstrap/esm/Card";
+
+type Project = {
+  name: string;
+};
+
+const App = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const res = await fetch("http://localhost:4000/projects");
+      setProjects(await res.json());
+    };
+    fetchProjects();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Projects</h1>
+
+      <Container>
+        {projects.map((project) => (
+          <Card>
+            <Card.Title>{project.name}</Card.Title>
+          </Card>
+        ))}
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
