afe825bf9f2b087f4c14866cde9401f54ff210e3
linode <linode>
2022-03-28 07:23:37 +0000

    logo

---
+++ website-ts/components/three/Logo.tsx
@@ -0,0 +1,36 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei"
+
+import styles from "/styles/components/three/Logo.module.scss";
+
+// three.js
+import Lights from "./Lights";
+import Model from "./Model"
+
+
+
+
+
+const Scene = () => {
+
+    
+
+    return (
+        <Canvas
+            shadows={true}
+            className={styles.canvas}
+            camera={{
+            position: [0, 0.5, 0.75],
+            }}
+        >
+            <Lights />
+            <OrbitControls />
+            <Suspense fallback={null}>
+                <Model path="/data/logo.glb" position={[0, 1.22, 1.78]}/>
+            </Suspense>
+        </Canvas>
+    )
+};
+
+export default Scene;
