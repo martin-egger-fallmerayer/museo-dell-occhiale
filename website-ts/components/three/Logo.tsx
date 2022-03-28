831ea76f4c9d5608ef292ec28c239427ec8f5f53
linode <linode>
2022-03-28 07:29:00 +0000

    logo

--- website-ts/components/three/Logo.tsx
+++ website-ts/components/three/Logo.tsx
@@ -27,7 +27,7 @@ const Scene = () => {
             <Lights />
             <OrbitControls />
             <Suspense fallback={null}>
-                <Model path="/data/logo.glb" position={[0, 1.22, 1.79]}/>
+                <Model path="/data/logo.glb" position={[0, .35, .5]}/>
             </Suspense>
         </Canvas>
     )
