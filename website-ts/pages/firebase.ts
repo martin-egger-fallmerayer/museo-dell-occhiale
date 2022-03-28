bc83850ebe3a670bd7f4d50b9201ab3cccc7beaa
steggma <steggmar@bx.fallmerayer.it>
2022-03-28 10:23:48 +0000

    fetch to firebase

--- website-ts/pages/firebase.ts
+++ website-ts/pages/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
