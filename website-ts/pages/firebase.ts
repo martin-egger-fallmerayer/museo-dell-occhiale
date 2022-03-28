5c0301e4fdb62da9c3f6b6d23a93d26692a78570
linode <linode>
2022-03-28 08:51:35 +0000

    firebase

---
+++ website-ts/pages/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5s2vo-pVMfe1s_pQHY4uiZRPis2NUezU",
+  authDomain: "database-visualization.firebaseapp.com",
+  projectId: "database-visualization",
+  storageBucket: "database-visualization.appspot.com",
+  messagingSenderId: "387405027701",
+  appId: "1:387405027701:web:4a3b23386c0bbfe68bdb3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
